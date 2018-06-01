c855f3c6a411f11eaf6e8c01610ff580598c32db
jesuarva <jean.ariza@jesuarva.com>
2018-06-02 00:18:01 +0200

    ADD GET to 'clients' and 'projects' endpoints via the routerFactory function

--- Routers/routerFactory.js
+++ Routers/routerFactory.js
@@ -1,28 +1,67 @@
 module.exports = function(router, db) {
   router
     .route('/')
-    .get(isIDValid, handleGet, handlerError)
-    .post(isIDValid, handlePost, handlerError);
+    .get(handleGet, handlerError)
+    .post(handlePost, handlerError);
 
   router
     .route('/:id')
-    .put(hanldePut, handlerError)
-    .delete(handleDelete, handlerError);
+    .put(isIDValid, hanldePut, handlerError)
+    .delete(isIDValid, handleDelete, handlerError);
 
   /**
    * HTTP request handlers: these are the handlers for HTTP Verbs.
    */
-  function handleGet(req, res, next) {}
+  function handleGet(req, res, next) {
+    db
+      .get()
+      .then(response => {
+        res.status(200).json(response);
+      })
+      .catch(e => {
+        emitError(e);
+        next(e);
+      });
+  }
   function handlePost(req, res, next) {}
   function hanldePut(req, res, next) {}
   function handleDelete(req, res, next) {}
   /**
    * Error handler: a middlewear to handle Erros.
    */
-  function handlerError(err, req, res, next) {}
+  function handlerError(err, req, res, next) {
+    res.status(err.status).json(err.message);
+    next();
+  }
   /**
    * Others: Helper middlewears
    */
-  function isIDValid(req, res, next) {}
+  function isIDValid(req, res, next) {
+    const { id } = req.params;
+    // id match only digits -> if 'id' is a number
+    if (/^\d+$/.test(id)) {
+      db
+        .get(Number(id))
+        .then(response => {
+          next();
+        })
+        .catch(e => {
+          emitError(e, 500);
+          next(e);
+        });
+    } else {
+      // Create an Erro instance to hanldle a custom error.
+      const e = new Error();
+      emitError(e, 400, 'Wrong Id: Id must be a number');
+      next(e);
+    }
+  }
   function areArgumentsValid(req, res, next) {}
+
+  //This function add some properties to an Error object
+  function emitError(e, code = 500, message = 'Ups, there were a problem fetching the info from the database') {
+    console.log(e);
+    e.status = code;
+    e.message = message;
+  }
 };
