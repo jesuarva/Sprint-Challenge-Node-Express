e207c85462034068d7ff739ba82840eea4a10c59
jesuarva <jean.ariza@jesuarva.com>
2018-06-02 00:21:35 +0200

    WIRE projectRouter.js

--- Routers/projectRouter.js
+++ Routers/projectRouter.js
@@ -2,4 +2,6 @@ const projects = require('express').Router();
 const db = require('../data/helpers/projectModel');
 const routerFactory = require('./routerFactory');
 
+routerFactory(projects, db);
+
 module.exports = projects;
