3902ac3ce821bdbf972dbceebcbc1f74dd29eda2
jesuarva <jean.ariza@jesuarva.com>
2018-06-01 22:54:52 +0200

    DEFINE starting Server

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-console.log('Server here');
+const express = require('express');
+
+const port = 6666;
+const server = express();
+
+server.listen(port, () => console.log('Server running on port %s ', port));
